ed8dcefba2d9b0d4b3e0019eda3aa3b2b24cf885
odtThomas <t.denotter@graafschapcollege.nl>
2017-12-20 17:12:12 +0100

    add random enemies

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+let Enemy = function(startX, startY) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = startX;
+    this.y = startY;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.speed = 250;
+    this.x += this.speed*dt;
+    // if enemy gets to the end of the canvas reset its position.
+    if(this.x > 600) {
+        this.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.reset = function() {
+    this.x = -100;
 };
 
 //Player class.
@@ -38,7 +42,7 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Player.prototype.handleInput = function(key){
-   //player movement
+   //player movement and check boundaries
     if(key === 'left' && this.x > 0){
        this.x -= 100;
    } else if (key === 'right' && this.x < 400) {
@@ -49,12 +53,17 @@ Player.prototype.handleInput = function(key){
        this.y += 90;
    }
 };
-// Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-// Now instantiate your objects.
 let allEnemies = [];
+const xValues = [-50,-110,-160,-200,-500,-350, -250,-410];
+const yValues = [60, 140, 220];
+for(let i =1;i<=8;i++){
+    let x = xValues[Math.floor(Math.random() * xValues.length)];
+    let y = yValues[Math.floor(Math.random() * yValues.length)]
+    allEnemies[i] = new Enemy(x, y);
+}
 let player = new Player(200,400);
 
 
