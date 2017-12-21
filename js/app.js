f4c04c8acf040552fcacc4910a2f69d31fdc355a
odtThomas <t.denotter@graafschapcollege.nl>
2017-12-21 11:13:26 +0100

    build leveling system with more and faster enemies

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 // Enemies our player must avoid
+let level = 1;
+let part = 1;
+
 let Enemy = function(startX, startY, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = startX;
@@ -33,15 +36,29 @@ let Player = function(startX, startY) {
     this.y = startY;
 };
 
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Player.prototype.update = function() {
+    document.getElementById("level").innerHTML = level;
+    document.getElementById("part").innerHTML = part;
+    if(this.y === -50) {
+        if(part === 3) {
+            part = 1;
+            level++;
+            createEnemies();
+        } else {
+            part++;
+        }
+        this.reset();
+    }
+
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+}
 Player.prototype.handleInput = function(key){
    //player movement and check boundaries
     if(key === 'left' && this.x > 0){
@@ -55,26 +72,33 @@ Player.prototype.handleInput = function(key){
    }
 };
 
-// Place all enemy objects in an array called allEnemies
+// create enemies array
 let allEnemies = [];
 // some default start positions
 const xValues = [-100,-225,-350,-450,-575,-675];
 const yValues = [60, 140, 220];
-for(let i =1;i<=8;i++){
-    // get a random start position from array above.
-    let x = xValues[Math.floor(Math.random() * xValues.length)];
-    let y = yValues[Math.floor(Math.random() * yValues.length)];
-    let speed;
-    // each has a diffirent speed. 1st row is slowest, last row the fastest.
-    if(y=== 60){
-        speed = 250;
-    } else if(y === 140) {
-        speed = 200;
-    } else {
-        speed = 150;
+createEnemies();
+
+// its in a function so the leveling system works. Higher a level, more enemies which are moving faster.
+function createEnemies() {
+    for(let i =1;i<=(level+8);i++){
+        // get a random start position from array above.
+        let x = xValues[Math.floor(Math.random() * xValues.length)];
+        let y = yValues[Math.floor(Math.random() * yValues.length)];
+        let speed;
+        // each has a diffirent speed. 1st row is slowest, last row the fastest.
+        if(y=== 60){
+            speed = (20*level)+250;
+        } else if(y === 140) {
+            speed = (20*level)+200;
+        } else {
+            speed = (20*level)+150;
+        }
+        // Place all enemy objects in an array called allEnemies
+        allEnemies[i] = new Enemy(x, y, speed);
     }
-    allEnemies[i] = new Enemy(x, y, speed);
 }
+console.log(allEnemies);
 // Place the player object in a variable called player
 let player = new Player(200,400);
 
